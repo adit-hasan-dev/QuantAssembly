36b7c9f827962b38d3f600a0082c0ab6e760fb7c
adit-hasan-dev <adit_hasan@icloud.com>
2024-12-25 23:51:44 -0800

    Refactor to use fluentui and implement position table

---
+++ QuantAssembly.Dashboard/Services/LedgerService.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using QuantAssembly.Dashboard.Models;
+
+public class LedgerService
+{
+    private IQueryable<Position> positions;
+
+    public async Task LoadLedgerFileAsync(Stream fileStream)
+    {
+        var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var positionList = await JsonSerializer.DeserializeAsync<List<Position>>(fileStream, options) ?? new List<Position>();
+            positions = positionList.ToArray().AsQueryable();
+    }
+
+    public IQueryable<Position> GetAllPositions()
+    {
+        return positions;
+    }
+}
