3003c54f5da09ae720ccfb3bdfeb649b05638bda
adit-hasan-dev <adit_hasan@icloud.com>
2025-01-03 17:21:48 -0800

    Add pie chart to dashboard page

--- QuantAssembly.Dashboard/Models/Position.cs
+++ QuantAssembly.Dashboard/Models/Position.cs
@@ -2,18 +2,19 @@ namespace QuantAssembly.Dashboard.Models
 {
     public class Position
     {
+        public double OpenPrice { get; set; }
+        public double ClosePrice { get; set; }
+        public double CurrentPrice { get; set; }
+        public double ProfitOrLoss { get; set; }
         public Guid PositionGuid { get; set; }
+        public string PlatformOrderId { get; set; }
         public string Symbol { get; set; }
         public string InstrumentType { get; set; }
         public string State { get; set; }
         public DateTime OpenTime { get; set; }
         public DateTime CloseTime { get; set; }
         public string Currency { get; set; }
-        public double OpenPrice { get; set; }
-        public double ClosePrice { get; set; }
-        public double CurrentPrice { get; set; }
         public int Quantity { get; set; }
-        public double ProfitOrLoss { get; set; }
         public string StrategyName { get; set; }
         public string StrategyDefinition { get; set; }
     }
--- QuantAssembly.Dashboard/Program.cs
+++ QuantAssembly.Dashboard/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using QuantAssembly.Dashboard;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<LedgerService>();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
